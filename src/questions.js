3d7f8545c1a0cb5ba26fa2afc6a29b174cc176fc
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-05-31 12:15:17 -0300

    add questions array

---
+++ src/questions.js
@@ -0,0 +1,20 @@
+export const questions = [
+    {
+        title: "What is the capital of France?",
+        options: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Madrid", correct: false },
+            { text: "Berlin", correct: false }
+        ]
+    },
+    {
+        title: "What is the longest river in the world?",
+        options: [
+            { text: "Nile", correct: false },
+            { text: "Amazon", correct: true },
+            { text: "Yangtze", correct: false },
+            { text: "Mississippi", correct: false }
+        ]
+    }
+];
