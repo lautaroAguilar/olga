e4e28e3ed4b34b8a133bb1788408e1ccaabe3735
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-05-31 12:18:12 -0300

    handle the answers

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
+import { questions } from "@/questions";
 const CustomCard = styled.div`
   width: 100%;
   height: 80%;
@@ -35,30 +35,91 @@ const CustomOption = styled.li`
   }
 `;
 const CustomTitle = styled.h2`
-  font-size: 28px;
+  font-size: 24px;
   text-align: center;
 `;
 const ContainerOptions = styled.div`
-  width: 100%;
-`;
-const CustomOptionList = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 1rem;
 `;
+const CustomOptionList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+const ContainerScore = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const CustomP = styled.p`
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
 export default function Card() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  /* const [isCorrect, setIsCorrect] = useState(false); */
+  const [isFinished, setIsFinished] = useState(false);
+
+  const handleAnswered = (correct, e) => {
+    const selectedOption = e.target;
+    const correctOption = Array.from(selectedOption.parentNode.children).find(
+      (option) =>
+        option.textContent ===
+        questions[currentQuestion].options.find((option) => option.correct).text
+    );
+
+    if (correct) {
+      setScore(score + 1);
+    }
+
+    selectedOption.style.background = correct ? "green" : "red";
+    correctOption.style.background = "green";
+    setTimeout(() => {
+      if (currentQuestion === questions.length - 1) {
+        setIsFinished(true);
+      } else {
+        setCurrentQuestion(currentQuestion + 1);
+      }
+    }, 3000);
+  };
+
   return (
     <CustomCard>
-      <CustomTitle>Title</CustomTitle>
-      <ContainerOptions>
-        <CustomOptionList>
-          <CustomOption>option 1</CustomOption>
-          <CustomOption>option 2</CustomOption>
-          <CustomOption>option 3</CustomOption>
-          <CustomOption>option 4</CustomOption>
-        </CustomOptionList>
-      </ContainerOptions>
+      {isFinished ? (
+        <>
+          <CustomTitle>Hiciste {score} {score > 1 ? "puntos" : "punto"}</CustomTitle>
+          <CustomOptionList>
+            <CustomP>¿Querés jugar de vuelta?</CustomP>
+          </CustomOptionList>
+        </>
+      ) : (
+        <>
+          <CustomTitle>{questions[currentQuestion].title}</CustomTitle>
+          <ContainerOptions>
+            <ContainerScore>
+              <CustomP>Puntuación {score}</CustomP>
+              <CustomP> {currentQuestion + 1} / 5</CustomP>
+            </ContainerScore>
+            <CustomOptionList>
+              {questions[currentQuestion].options.map((question) => (
+                <CustomOption
+                  key={question.text}
+                  onClick={(e) => handleAnswered(question.correct, e)}
+                >
+                  {question.text}
+                </CustomOption>
+              ))}
+            </CustomOptionList>
+          </ContainerOptions>
+        </>
+      )}
     </CustomCard>
   );
 }
