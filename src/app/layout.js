bd1a82edc6d6b0343d8adda2365545fee859936d
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-06-05 17:44:15 -0300

    fix layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,43 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import AppContextProvider from "@/context/useContext";
+import Cloud from "@/components/Cloud/Cloud";
+import { Clouds } from "@/components/Clouds/Clouds";
+import Image from "next/image";
+import logo from "../../public/soñe-que-volaba.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Olga Quiz",
+  title: "SQV Quiz",
   description: "El juego de preguntas y respuestas sobre Soñé que volaba",
-  visualViewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+  visualViewport:
+    "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AppContextProvider>
+        <body
+          className={inter.className}
+          style={{
+            height: "100dvh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            background: "#016cb2",
+            padding: "1rem",
+            position: "relative",
+          }}
+        >
+          <Image src={logo} alt="Logo de Olga" style={{ zIndex: 50 }} />
+          {children}
+          {Clouds.map((cloud) => (
+            <Cloud key={cloud.id} style={cloud.styles} />
+          ))}
+        </body>
+      </AppContextProvider>
     </html>
   );
 }
