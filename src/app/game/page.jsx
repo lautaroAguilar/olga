62cce170d8bed3a26bb86eb4f4e6f42278a40e8a
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-06-05 17:44:59 -0300

    create new page game and clean up Home

---
+++ src/app/game/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Card from '@/components/Card/Card'
+export default function GamePage() {
+  return (
+    <Card/>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,10 @@
 "use client";
-import Card from "@/components/Card/Card";
-import styled from "styled-components";
-import Image from "next/image";
-import logo from "../../public/soñe-que-volaba.png";
-import Cloud from "@/components/Cloud/Cloud";
-import { Clouds } from "@/components/Clouds/Clouds";
-const CustomMain = styled.main`
-  height: 100dvh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background: #016cb2;
-  padding: 2rem;
-  position: relative;
-`;
+import Intro from "@/components/Intro/Intro";
 
 export default function Home() {
   return (
-    <CustomMain>
-      <Image src={logo} alt="Logo de Olga" style={{ zIndex: 50 }} />
-      <Card />
-
-      {Clouds.map((cloud) => (
-        <Cloud key={cloud.id} style={cloud.styles} />
-      ))}
-    </CustomMain>
+    <>
+      <Intro />
+    </>
   );
 }
