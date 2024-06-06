e0514afe566cf32a7f0ba1546e25c944779d7724
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-06-06 09:13:33 -0300

    recreate structure questions

--- src/questions.js
+++ src/questions.js
@@ -1,92 +1,90 @@
-export const questions = [
+export const questions = {
+  easy: [
     {
-        level: "fácil",
-        title: "¿Cuál es el color del cielo en un día despejado?",
-        options: [
-            { text: "Azul", correct: true },
-            { text: "Verde", correct: false },
-            { text: "Rojo", correct: false },
-            { text: "Amarillo", correct: false }
-        ]
+      title: "¿Cuál es el color del cielo en un día despejado?",
+      options: [
+        { text: "Azul", correct: true },
+        { text: "Verde", correct: false },
+        { text: "Rojo", correct: false },
+        { text: "Amarillo", correct: false },
+      ],
     },
     {
-        level: "fácil",
-        title: "¿Cuántos días tiene el mes de febrero en un año bisiesto?",
-        options: [
-            { text: "29", correct: true },
-            { text: "30", correct: false },
-            { text: "28", correct: false },
-            { text: "31", correct: false }
-        ]
+      title: "¿Cuántos días tiene el mes de febrero en un año bisiesto?",
+      options: [
+        { text: "29", correct: true },
+        { text: "30", correct: false },
+        { text: "28", correct: false },
+        { text: "31", correct: false },
+      ],
     },
     {
-        level: "fácil",
-        title: "¿Cuál es el animal terrestre más grande del mundo?",
-        options: [
-            { text: "Elefante", correct: false },
-            { text: "Jirafa", correct: false },
-            { text: "Rinoceronte", correct: false },
-            { text: "Elefante Africano", correct: true }
-        ]
+      title: "¿Cuál es el animal terrestre más grande del mundo?",
+      options: [
+        { text: "Elefante", correct: false },
+        { text: "Jirafa", correct: false },
+        { text: "Rinoceronte", correct: false },
+        { text: "Elefante Africano", correct: true },
+      ],
+    },
+  ],
+  medium: [
+    {
+      title: "¿Cuál es el color del cielo en un día despejado?",
+      options: [
+        { text: "Azul", correct: true },
+        { text: "Verde", correct: false },
+        { text: "Rojo", correct: false },
+        { text: "Amarillo", correct: false },
+      ],
     },
     {
-        level: "medio",
-        title: "¿Cuál es la capital de Italia?",
-        options: [
-            { text: "París", correct: false },
-            { text: "Roma", correct: true },
-            { text: "Madrid", correct: false },
-            { text: "Berlín", correct: false }
-        ]
+      title: "¿Cuántos días tiene el mes de febrero en un año bisiesto?",
+      options: [
+        { text: "29", correct: true },
+        { text: "30", correct: false },
+        { text: "28", correct: false },
+        { text: "31", correct: false },
+      ],
     },
     {
-        level: "medio",
-        title: "¿Cuál es el océano más grande del mundo?",
-        options: [
-            { text: "Océano Atlántico", correct: false },
-            { text: "Océano Índico", correct: false },
-            { text: "Océano Pacífico", correct: true },
-            { text: "Océano Ártico", correct: false }
-        ]
+      title: "¿Cuál es el animal terrestre más grande del mundo?",
+      options: [
+        { text: "Elefante", correct: false },
+        { text: "Jirafa", correct: false },
+        { text: "Rinoceronte", correct: false },
+        { text: "Elefante Africano", correct: true },
+      ],
+    },
+  ],
+  hard: [
+    {
+      title: "¿Cuál es la fórmula química del agua?",
+      options: [
+        { text: "H2O", correct: true },
+        { text: "CO2", correct: false },
+        { text: "NaCl", correct: false },
+        { text: "CH4", correct: false },
+      ],
     },
     {
-        level: "medio",
-        title: "¿Quién escribió la obra 'Don Quijote de la Mancha'?",
-        options: [
-            { text: "Miguel de Cervantes", correct: true },
-            { text: "William Shakespeare", correct: false },
-            { text: "Gabriel García Márquez", correct: false },
-            { text: "Julio Cortázar", correct: false }
-        ]
+      title: "¿Cuál es el océano más grande del mundo?",
+      options: [
+        { text: "Océano Atlántico", correct: false },
+        { text: "Océano Índico", correct: false },
+        { text: "Océano Pacífico", correct: true },
+        { text: "Océano Ártico", correct: false },
+      ],
     },
     {
-        level: "difícil",
-        title: "¿Cuál es la fórmula química del agua?",
-        options: [
-            { text: "H2O", correct: true },
-            { text: "CO2", correct: false },
-            { text: "NaCl", correct: false },
-            { text: "CH4", correct: false }
-        ]
+      title:
+        "¿En qué año se firmó la Declaración de Independencia de los Estados Unidos?",
+      options: [
+        { text: "1776", correct: true },
+        { text: "1789", correct: false },
+        { text: "1800", correct: false },
+        { text: "1812", correct: false },
+      ],
     },
-    {
-        level: "difícil",
-        title: "¿En qué año se firmó la Declaración de Independencia de los Estados Unidos?",
-        options: [
-            { text: "1776", correct: true },
-            { text: "1789", correct: false },
-            { text: "1800", correct: false },
-            { text: "1812", correct: false }
-        ]
-    },
-    {
-        level: "difícil",
-        title: "¿Cuál es la montaña más alta del mundo?",
-        options: [
-            { text: "Everest", correct: true },
-            { text: "Kilimanjaro", correct: false },
-            { text: "Aconcagua", correct: false },
-            { text: "K2", correct: false }
-        ]
-    }
-];
+  ],
+};
