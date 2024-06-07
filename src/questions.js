2c0ce0e00c6f5bfccb2b336ca76727c50cfa3146
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-06-06 21:04:29 -0300

    add questions

--- src/questions.js
+++ src/questions.js
@@ -1,89 +1,145 @@
 export const questions = {
   easy: [
     {
-      title: "¿Cuál es el color del cielo en un día despejado?",
+      title: "¿Quién es el conductor?",
       options: [
-        { text: "Azul", correct: true },
-        { text: "Verde", correct: false },
-        { text: "Rojo", correct: false },
-        { text: "Amarillo", correct: false },
+        { text: "Roque Rincón", correct: false },
+        { text: `Bautista "El Vulva" Bulbini`, correct: false },
+        { text: "Miguel Granados", correct: true },
+        { text: "El Rudo Roncaglia", correct: false },
       ],
     },
     {
-      title: "¿Cuántos días tiene el mes de febrero en un año bisiesto?",
+      title: "¿Cómo se compone el logo de Soñé que Volaba?",
       options: [
-        { text: "29", correct: true },
-        { text: "30", correct: false },
-        { text: "28", correct: false },
-        { text: "31", correct: false },
+        { text: "Una zapatilla Jordan", correct: false },
+        { text: `Un cocodrilo con gafas`, correct: false },
+        { text: `Una nube y una "V corta"`, correct: true },
+        { text: "Una guitarra electrica", correct: false },
       ],
     },
     {
-      title: "¿Cuál es el animal terrestre más grande del mundo?",
+      title: "¿A qué famoso jugador de fútbol entrevistaron en 2023?",
       options: [
-        { text: "Elefante", correct: false },
-        { text: "Jirafa", correct: false },
-        { text: "Rinoceronte", correct: false },
-        { text: "Elefante Africano", correct: true },
+        { text: "El Pipi Romagnoli", correct: false },
+        { text: "Kylian Mbappé", correct: false },
+        { text: "Michael Jordan", correct: false },
+        { text: "Lionel Andrés Messi Cuccittini", correct: true },
+      ],
+    },
+    {
+      title: "¿Por qué clase de eventos es reconocido Soñé que Volaba y Olga?",
+      options: [
+        { text: `Campeonatos mundiales de "veo veo"`, correct: false },
+        { text: "Shows musicales", correct: true },
+        { text: "La Copa Piston", correct: false },
+        { text: "Todas son correctas", correct: false },
+      ],
+    },
+    {
+      title: "¿Cual es la actividad favorita de Lucas Fridman?",
+      options: [
+        { text: "Ayudar en comedores", correct: false },
+        { text: "Donar a la caridad", correct: false },
+        { text: "Atrasar", correct: true },
+        { text: "Repartir dinero around the world", correct: false },
       ],
     },
   ],
   medium: [
     {
-      title: "¿Cuál es el color del cielo en un día despejado?",
+      title: "¿Qué dia comenzó el programa?",
       options: [
-        { text: "Azul", correct: true },
-        { text: "Verde", correct: false },
-        { text: "Rojo", correct: false },
-        { text: "Amarillo", correct: false },
+        { text: "12 de Junio", correct: true },
+        { text: "28 de Diciembre", correct: false },
+        { text: "14 de Abril", correct: false },
+        { text: "12 de Julio", correct: false },
       ],
     },
     {
-      title: "¿Cuántos días tiene el mes de febrero en un año bisiesto?",
+      title: `¿Quiénes integraban la "primer mesa"?`,
       options: [
-        { text: "29", correct: true },
-        { text: "30", correct: false },
-        { text: "28", correct: false },
-        { text: "31", correct: false },
+        { text: "Migue Granados, Lucas Fridman y Gime Accardi", correct: false },
+        { text: "Lucas Fridman, Gime Accardi y Homero Petinatto", correct: false },
+        { text: "Migue Granados, Lucas Fridman y Tefi Russo", correct: false },
+        { text: "Migue Granados, Julián Lucero y Sofi Morandi", correct: true },
       ],
     },
     {
-      title: "¿Cuál es el animal terrestre más grande del mundo?",
+      title: "¿Qué característica distintiva tiene el estudio?",
       options: [
-        { text: "Elefante", correct: false },
-        { text: "Jirafa", correct: false },
-        { text: "Rinoceronte", correct: false },
-        { text: "Elefante Africano", correct: true },
+        { text: "Paredes con ladrillo a la vista", correct: false },
+        { text: "Un gran ventanal", correct: true },
+        { text: "Una fuente de agua", correct: false },
+        { text: "El jardín vertical", correct: false },
+      ],
+    },
+    {
+      title: `¿Sobre qué artista fue el primer "DAY"?`,
+      options: [
+        { text: "Los redondos", correct: false },
+        { text: "Gustavo Cerati", correct: true },
+        { text: "Damas Gratis", correct: false },
+        { text: "Mercedes Sosa", correct: false },
+      ],
+    },
+    {
+      title: `¿Qué banda creó la cortina musical del programa?`,
+      options: [
+        { text: `Conociendo Rusia`, correct: false },
+        { text: `Turf`, correct: false },
+        { text: `Ella es tan cargosa`, correct: true },
+        { text: `El Kuelgue`, correct: false },
       ],
     },
   ],
   hard: [
     {
-      title: "¿Cuál es la fórmula química del agua?",
+      title: `¿Quién fue el/la primer artista en visitar SQV?`,
       options: [
-        { text: "H2O", correct: true },
-        { text: "CO2", correct: false },
-        { text: "NaCl", correct: false },
-        { text: "CH4", correct: false },
+        { text: "Joaquin Levinton", correct: false },
+        { text: "Patricia Sosa", correct: false },
+        { text: "Mateo Sujatovich", correct: true },
+        { text: "Pablito Lescano", correct: false },
       ],
     },
     {
-      title: "¿Cuál es el océano más grande del mundo?",
+      title: `¿En qué fecha se realizo el "segundo" show en el Movistar Arena?`,
       options: [
-        { text: "Océano Atlántico", correct: false },
-        { text: "Océano Índico", correct: false },
-        { text: "Océano Pacífico", correct: true },
-        { text: "Océano Ártico", correct: false },
+        { text: "11 de Diciembre", correct: true },
+        { text: "12 de Diciembre", correct: false },
+        { text: "13 de Diciembre", correct: false },
+        { text: "14 de Diciembre", correct: false },
       ],
     },
     {
       title:
-        "¿En qué año se firmó la Declaración de Independencia de los Estados Unidos?",
+        "¿Quién fue el primer invitado del ciclo 2024?",
       options: [
-        { text: "1776", correct: true },
-        { text: "1789", correct: false },
-        { text: "1800", correct: false },
-        { text: "1812", correct: false },
+        { text: "Damian Betular", correct: false },
+        { text: "Roberto Galati", correct: false },
+        { text: "Duki", correct: true },
+        { text: "Francis Mallmann", correct: false },
+      ],
+    },
+    {
+      title:
+        "¿Qué hecho peligroso y trascendente ocurrió en la esquina del canal?",
+      options: [
+        { text: "Un incendio", correct: false },
+        { text: "Un robo", correct: false },
+        { text: "Un choque de autos", correct: true },
+        { text: "Una explosión", correct: false },
+      ],
+    },
+    {
+      title:
+        "¿Quién fue el primer columnista de SQV?",
+      options: [
+        { text: "Diego Pereyra", correct: true },
+        { text: `Martin "El corvinon" Reich`, correct: false },
+        { text: "Homero Petinatto", correct: false },
+        { text: "Ximena Diaz Alarcón", correct: false },
       ],
     },
   ],
